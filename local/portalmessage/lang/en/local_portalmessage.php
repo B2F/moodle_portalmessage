76f444a783b9ad1a239db2a2dd1d9a27cb0c1baf
B2F <didier.boff@hotmail.fr>
2026-04-09 15:17:33 +0200

    Initial commit of portal message local config

---
+++ local/portalmessage/lang/en/local_portalmessage.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['pluginname'] = 'Portal message';
+$string['enabled'] = 'Enabled';
+$string['enabled_desc'] = 'Show the portal message block when enabled.';
+$string['message'] = 'Message text';
+$string['message_desc'] = 'The text shown in the portal message banner.';
+$string['messageversion'] = 'Message version';
+$string['messageversion_desc'] = 'Increment this when the message content changes so dismissals are reset.';
+$string['messagetype'] = 'Message type';
+$string['messagetype_desc'] = 'Choose the presentation style for the portal message.';
+$string['messagetype_info'] = 'Info';
+$string['messagetype_warning'] = 'Warning';
+$string['targetcapability'] = 'Target capability';
+$string['targetcapability_desc'] = 'Only users with this capability can see the portal message.';
---
+++ local/portalmessage/settings.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($hassiteconfig) {
+    $settings = new admin_settingpage('local_portalmessage', get_string('pluginname', 'local_portalmessage'));
+
+    if ($ADMIN->fulltree) {
+        $settings->add(new admin_setting_configcheckbox(
+            'local_portalmessage/enabled',
+            get_string('enabled', 'local_portalmessage'),
+            get_string('enabled_desc', 'local_portalmessage'),
+            1
+        ));
+
+        $settings->add(new admin_setting_configtextarea(
+            'local_portalmessage/message',
+            get_string('message', 'local_portalmessage'),
+            get_string('message_desc', 'local_portalmessage'),
+            ''
+        ));
+
+        $settings->add(new admin_setting_configselect(
+            'local_portalmessage/messagetype',
+            get_string('messagetype', 'local_portalmessage'),
+            get_string('messagetype_desc', 'local_portalmessage'),
+            'info',
+            [
+                'info' => get_string('messagetype_info', 'local_portalmessage'),
+                'warning' => get_string('messagetype_warning', 'local_portalmessage'),
+            ]
+        ));
+
+        $settings->add(new admin_setting_configtext(
+            'local_portalmessage/targetcapability',
+            get_string('targetcapability', 'local_portalmessage'),
+            get_string('targetcapability_desc', 'local_portalmessage'),
+            'moodle/site:config',
+            PARAM_RAW_TRIMMED
+        ));
+
+        $settings->add(new admin_setting_configtext(
+            'local_portalmessage/messageversion',
+            get_string('messageversion', 'local_portalmessage'),
+            get_string('messageversion_desc', 'local_portalmessage'),
+            1,
+            PARAM_INT
+        ));
+    }
+
+    $ADMIN->add('localplugins', $settings);
+}
